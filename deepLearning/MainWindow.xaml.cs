c26074e0de934a5656925a649e06c832e962967b
MuhsinFatih <muhsinfatih@gmail.com>
2017-05-17 19:49:39 +0300

    commit

--- deepLearning/MainWindow.xaml.cs
+++ deepLearning/MainWindow.xaml.cs
@@ -16,128 +16,90 @@ using System.ComponentModel;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media.Animation;
 
-namespace deepLearning
-{
+namespace deepLearning {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
             this.ContentRendered += main;
         }
 
 
-
-
-
         
-
-
-
         int pixelSize = 70;
         int neuronSize = 52;
         List<Layer> layers = new List<Layer>();
 
 
-        Dictionary<string, Vector> offset;
+        Dictionary<string, Vector> offset = new Dictionary<string, Vector>();
 
-        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            offset["grid_neurons"] = VisualTreeHelper.GetOffset(grid_neurons);
-            offset["layer0"] = VisualTreeHelper.GetOffset(grid_layer0); // input visual representation layer
+        Vector getVector(UIElement of, UIElement relativeTo) {
+            return (Vector)of.TransformToAncestor(relativeTo).Transform(new Point(0, 0));
+        }
+
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e) {
+            offset["grid_neurons"] = getVector(of: grid_neurons, relativeTo: mainCanvas);
+            offset["layer0"] = getVector(of: grid_layer0, relativeTo: mainCanvas); // input visual representation layer
+            offset["inputRect"] = getVector(of: inputRect, relativeTo: mainCanvas) + offset["layer0"]; // input visual representation rectangle (the pixels)
 
             // sum vectors to get real positions in canvas
-            offset["layer1"] = VisualTreeHelper.GetOffset(grid_layer1) + offset["grid_neurons"]; // input layer
-            offset["layer2"] = VisualTreeHelper.GetOffset(grid_layer2) + offset["grid_neurons"];
-            offset["layer3"] = VisualTreeHelper.GetOffset(grid_layer3) + offset["grid_neurons"];
-            offset["layer4"] = VisualTreeHelper.GetOffset(grid_layer4) + offset["grid_neurons"]; // output layer
+            offset["layer1"] = getVector(of: grid_layer1, relativeTo: mainCanvas); // input layer
+            offset["layer2"] = getVector(of: grid_layer2, relativeTo: mainCanvas);
+            offset["layer3"] = getVector(of: grid_layer3, relativeTo: mainCanvas);
+            offset["layer4"] = getVector(of: grid_layer4, relativeTo: mainCanvas); // output layer
+
+
+            foreach (Neuron item in grid_layer1.Children) {
+				offset[item.Name] = getVector(of: item, relativeTo: mainCanvas);
+			}
+            foreach (var item in offset) {
+                Console.WriteLine("offset[{0}] = {1}:{2}", item.Key, item.Value.X, item.Value.Y);
+            }
         }
-        public void main(object sender, EventArgs e)
-        {
+        public void main(object sender, EventArgs e) {
 
 
-            Vector offset = VisualTreeHelper.GetOffset(inputRect);
-            Vector offset2 = VisualTreeHelper.GetOffset(btn2);
+            //    Vector relativePoint = (Vector)n1.TransformToAncestor(mainCanvas).Transform(new Point(0, 0));
+            //    Point relativePoint2 = n1.TransformToAncestor(mainCanvas).Transform(new Point(0, 0));
 
-            Console.WriteLine("left = " + offset.X);
-            Console.WriteLine("left2 = " + offset2.X);
+            //    Console.WriteLine("relativePoint = {0}", relativePoint);
 
-
-
-
-            var canvasHeight = mainCanvas.ActualHeight;
-            var r = new List<Rectangle>
-                {
-                    rect(pixelSize, 10, canvasHeight / 2 - pixelSize),
-                    rect(pixelSize, 10, canvasHeight / 2),
-                    rect(pixelSize, 10 + 70, canvasHeight / 2 - pixelSize),
-                    rect(pixelSize, 10 + 70, canvasHeight / 2)
-                };
-            for (int i = 0; i < r.Count; ++i)
-            {
-                mainCanvas.Children.Add(r[i]);
-            }
-
-            layers.Add(new Layer(name : "input"));
-            for (int i = 0; i < r.Count; ++i)
-            {
-                layers[0].neurons.Add(new Neuron(neuronSize));
-                Canvas.SetTop(layers[0].neurons[i].ellipse, canvasHeight / 4 * i + canvasHeight / 8 - neuronSize / 2);
-                Canvas.SetLeft(layers[0].neurons[i].ellipse, 200);
-                mainCanvas.Children.Add(layers[0].neurons[i].ellipse);
-            }
-            for (int i = 0; i < layers.Count; ++i)
-            {
-
-            }
+            //    Line line = new Line() {
+            //        Stroke = Brushes.Black,
+            //        StrokeThickness = 2,
+            //        X1 = 0,
+            //        Y1 = 0,
+            //        X2 = relativePoint.X,
+            //        Y2 = relativePoint.Y
+            //    };
+            //    mainCanvas.Children.Add(line);
         }
 
 
 
-        Rectangle rect(double pixelSize, double left, double right)
-        {
-            
-            Rectangle r = new Rectangle()
-            {
-                Width = this.pixelSize,
-                Height = this.pixelSize,
-                Stroke = Brushes.Black,
-                StrokeThickness = 2,
-                Fill = Brushes.White
-            };
-            Canvas.SetLeft(r, left); // margin
-            Canvas.SetTop(r, right);
-            return r;
-        }
-
-        
     }
 
-    struct Layer
-    {
+    struct Layer {
         public string name;
         public List<Neuron> neurons;
-        public Layer(string name)
-        {
+        public Layer(string name) {
             this.name = name;
             neurons = new List<Neuron>();
         }
     }
 
-    class Neuron
-    {
-        public double weight;
-        public double value;
-        public Ellipse ellipse;
+    //class Neuron : Border{
+    //    public double weight;
+    //    public double value;
+    //    public Ellipse ellipse;
 
-        public Neuron(int neuronSize)
-        {
-            weight = 0;
-            value = 0;
-            ellipse = new Ellipse() { Width = neuronSize, Height = 52, Fill = Brushes.White, Stroke = Brushes.Black, StrokeThickness = 1 };
-        }
-    }
+
+    //    public Neuron(int neuronSize) {
+    //        weight = 0;
+    //        value = 0;
+    //        ellipse = new Ellipse() { Width = neuronSize, Height = 52, Fill = Brushes.White, Stroke = Brushes.Black, StrokeThickness = 1 };
+    //    }
+    //}
 }
