d9e5537cf8c62e95b9d37e6260490c8e4234e25c
MuhsinFatih <muhsinfatih@gmail.com>
2017-05-17 03:24:56 +0300

    commit

--- deepLearning/MainWindow.xaml.cs
+++ deepLearning/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
 
 namespace deepLearning
 {
@@ -23,6 +26,45 @@ namespace deepLearning
         public MainWindow()
         {
             InitializeComponent();
+            
+
+
+
+
+
+
+
         }
+        private void onMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            UIElement element = sender as UIElement;
+            if (element == null)
+                return;
+            DragDrop.DoDragDrop(element, new DataObject(this), DragDropEffects.Move);
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hello");
+            Line line = new Line();
+            myCanvas.Children.Add(line);
+            line.Stroke = Brushes.Red;
+            line.StrokeThickness = 2;
+            line.X1 = 0;
+            line.Y1 = 0;
+            Storyboard sb = new Storyboard();
+            DoubleAnimation da = new DoubleAnimation(line.Y2, 100, new Duration(new TimeSpan(0, 0, 1)));
+            DoubleAnimation da1 = new DoubleAnimation(line.X2, 100, new Duration(new TimeSpan(0, 0, 1)));
+            Storyboard.SetTargetProperty(da, new PropertyPath("(Line.Y2)"));
+            Storyboard.SetTargetProperty(da1, new PropertyPath("(Line.X2)"));
+            sb.Children.Add(da);
+            sb.Children.Add(da1);
+
+            line.BeginStoryboard(sb);
+
+
+        }
+
+
+
     }
 }
